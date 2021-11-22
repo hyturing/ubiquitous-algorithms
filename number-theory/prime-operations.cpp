fed78acd40247e3ec3c0c16833969b608eb77071
Hemant Kumar Yadav <hyturing@gmail.com>
2021-11-22 10:36:30 +0530

    added prime

--- number-theory/prime-operations.cpp
+++ number-theory/prime-operations.cpp
@@ -2,15 +2,32 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+int power[100000]={0};
+
 bool prime(int n){
 	if (n<2) return false;
 
 	for(int i = 2; i*i <= n; i++){
 		if(n%i == 0) return false;
 	}
-
 	return true;
 }
+
+vector<int> factors(int n){
+	vector<int> f;
+	for(int i = 2; i*i <= n; i++){
+		while(n%i == 0){
+			f.push_back(i);
+			n /= i;
+			power[i]++;
+		}
+	}
+
+	if(n > 1){f.push_back(n); power[n]++;}
+
+	return f;
+}
+
 
 int32_t main(){
 	ios_base::sync_with_stdio(0);
@@ -21,5 +38,12 @@ int32_t main(){
 	cin >> n;
 
 	cout << (prime(n) ? "Yes": "No") << endl;
+
+	vector<int> f;
+
+	f = factors(n);
+	for(auto x: f) cout << x << " "; cout << endl;
+	// for(auto x: power) if(x!=0) cout << x << " ";
+	
 	return 0;
 }
